2d895e2aaed8b97e86067162fddae10284759a1e
HeonjiHwang <49047924+HeonjiHwang@users.noreply.github.com>
2022-05-16 09:59:46 +0900

    test

--- vueJS/meta-mask-test/src/components/MetaMaskTest.vue
+++ vueJS/meta-mask-test/src/components/MetaMaskTest.vue
@@ -1,15 +1,48 @@
 <script setup>
-  import Web3 from 'web3';
+  import {ethers} from 'ethers';
+  import {ref} from 'vue';
+  import solidityInfo from '../util/solidityInfo';
+  
+  let result = ref(0);
+  let grounds = ref([]);
 
   const onCheck = () =>{
-    const web3 = new Web3(window.ethereum);
-    web3.eth.getAccounts(console.log)
+    let account = process.env.VUE_APP_MATAMASK_ACCOUNT;
+    let provider = new ethers.providers.Web3Provider(window.ethereum);
+    let balanceEth = 0, usd = 0;
+
+    provider.getBalance(account).then((balance)=>{ 
+      console.log(balance);
+      balanceEth = ethers.utils.formatEther(balance);
+    })
+
+
+    fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum").then((res)=>{
+      if(res.status !== 200){
+        return;
+      }
+      
+      res.json().then((data)=>{
+        let currentPrice = data[0].current_price;
+        usd = currentPrice * balanceEth;
+
+        result.value = balanceEth + 'ETH  ' + usd + 'USD'
+      })
+    })
+  }
+
+  const getGround = ()=>{
+    solidityInfo.geContract();
   }
 </script>
 
 <template>
   <div>
-    <button @click="onCheck">확인</button>
-    <button @click="onSignout">로그아웃</button>
+    <button @click="onCheck">확인</button><br>
+    <span>{{result}}</span><br>
+    <button @click="getGround">내 그라운드</button> <br>
+    <span v-for="(ground, idx) in grounds" :key="idx">
+      {{ground}}
+    </span>
   </div>
 </template>
